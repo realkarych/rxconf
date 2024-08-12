360b2a3c1a4e83c23776bce4c94ac63ae0b50aaa
Andrey Karchevsky <karych@karych.ru>
2024-08-12 21:51:12 +0300

    :white_check_mark: Update import test

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,26 +1,22 @@
-def test_attributes_import():
-    try:
-        from rxconf import attributes  # noqa: F401
-    except Exception as e:
-        assert False, f"Importing attributes.py raised an exception: {e}"
+import importlib
+import os
+import pytest
 
 
-def test_config_types_import():
-    try:
-        from rxconf import config_types  # noqa: F401
-    except Exception as e:
-        assert False, f"Importing config_types.py raised an exception: {e}"
+def test_import_all_modules():
+    package_name = "rxconf"
+    module = importlib.import_module(package_name)
+    package_dir = getattr(module, "__file__", None)
 
+    if package_dir is None:
+        pytest.fail(f"Failed to determine the directory for package {package_name}")
 
-def test_exceptions_import():
-    try:
-        from rxconf import exceptions  # noqa: F401
-    except Exception as e:
-        assert False, f"Importing exceptions.py raised an exception: {e}"
+    package_dir = os.path.dirname(package_dir)
 
-
-def test_rxconf_import():
-    try:
-        import rxconf  # noqa: F401
-    except Exception as e:
-        assert False, f"Importing rxconf entry-point raised an exception: {e}"
+    for filename in os.listdir(package_dir):
+        if filename.endswith(".py") and filename != "__init__.py":
+            module_name = f"{package_name}.{filename[:-3]}"
+            try:
+                importlib.import_module(module_name)
+            except Exception as e:
+                pytest.fail(f"Importing {module_name} raised an exception: {e}")
