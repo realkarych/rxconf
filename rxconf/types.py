a7e005fd7e5eeba3583b1b88a1e96f8b360d9608
Andrey Karchevsky <karych@karych.ru>
2024-08-13 01:13:46 +0300

    :truck: Moved data processing

--- rxconf/types.py
+++ rxconf/types.py
@@ -1,4 +1,3 @@
-import datetime
 import os
 import typing as tp
 from abc import ABCMeta, abstractmethod
@@ -7,7 +6,7 @@ from pathlib import PurePath
 import yaml
 
 from rxconf import attributes as attrs
-from rxconf import exceptions
+from rxconf import data_processors, exceptions
 
 
 class ConfigType(metaclass=ABCMeta):
@@ -50,38 +49,5 @@ class YamlConfig(FileConfigType):
                 ) from exc
 
         return cls(
-            root_attribute=_YamlDataProcessor(data=yaml_data).start_processing()
+            root_attribute=data_processors.YamlDataProcessor(data=yaml_data).start_processing()
         )
-
-
-class _YamlDataProcessor:
-
-    def __init__(self, data: tp.Any) -> None:
-        self._data = data
-
-    def start_processing(self) -> attrs.YamlAttribute:
-        return self._process_data(self._data)
-
-    def _process_data(self, data: tp.Any) -> attrs.YamlAttribute:
-        if isinstance(data, dict):
-            return attrs.YamlAttribute(
-                value={k: self._process_data(v) for k, v in data.items()}
-            )
-        elif isinstance(data, list):
-            return attrs.YamlAttribute(
-                value=[
-                    self._process_data(item) if not isinstance(item, dict) else attrs.YamlAttribute(
-                        value={k: self._process_data(v) for k, v in item.items()}
-                    ) for item in data
-                ]
-            )
-        elif isinstance(data, set):
-            return attrs.YamlAttribute(
-                value={self._process_data(item) for item in data}
-            )
-        elif isinstance(data, (str, int, float, bool, type(None))):
-            return attrs.YamlAttribute(value=data)
-        elif isinstance(data, (datetime.date, datetime.datetime)):
-            return attrs.YamlAttribute(value=data)
-        else:
-            raise exceptions.BrokenConfigSchemaError(f"Unsupported data type: {type(data)}")
