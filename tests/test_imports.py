34187c9aad75d38ee21cf89bb7716cad96409ae6
Andrey Karchevsky <karych@karych.ru>
2024-08-14 01:32:10 +0300

    :white_check_mark: Update import test

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,22 +1,23 @@
 import importlib
-import os
+import pkgutil
+
 import pytest
 
 
 def test_import_all_modules():
     package_name = "rxconf"
-    module = importlib.import_module(package_name)
-    package_dir = getattr(module, "__file__", None)
+    try:
+        module = importlib.import_module(package_name)
+    except ImportError as e:
+        pytest.fail(f"Failed to import package {package_name}: {e}")
+
+    package_dir = getattr(module, "__path__", None)
 
     if package_dir is None:
         pytest.fail(f"Failed to determine the directory for package {package_name}")
 
-    package_dir = os.path.dirname(package_dir)
-
-    for filename in os.listdir(package_dir):
-        if filename.endswith(".py") and filename != "__init__.py":
-            module_name = f"{package_name}.{filename[:-3]}"
-            try:
-                importlib.import_module(module_name)
-            except Exception as e:
-                pytest.fail(f"Importing {module_name} raised an exception: {e}")
+    for _, module_name, _ in pkgutil.walk_packages(package_dir, package_name + "."):
+        try:
+            importlib.import_module(module_name)
+        except Exception as e:
+            pytest.fail(f"Importing {module_name} raised an exception: {e}")
