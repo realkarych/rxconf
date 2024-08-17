9685ed3d3dabe35e5247dd75429f318fe4546292
Andrey Karchevsky <karych@karych.ru>
2024-08-17 14:08:07 +0300

    :white_check_mark: Migrated end-to-end, add units for types & attributes

---
+++ tests/unit/test_types.py
@@ -0,0 +1,30 @@
+import unittest
+from unittest.mock import patch, MagicMock
+import sys
+
+
+class TestTypeAliasImport(unittest.TestCase):
+
+    @patch('sys.version_info', new=(3, 9))
+    def test_import_typealias_from_typing_extensions(self):
+        with patch.dict('sys.modules', {'typing_extensions': MagicMock()}):
+            if 'rxconf.types' in sys.modules:
+                del sys.modules['rxconf.types']
+            import rxconf.types
+            self.assertTrue(hasattr(rxconf.types, 'TypeAlias'))
+
+    @patch('sys.version_info', new=(3, 10))
+    def test_import_typealias_from_typing(self):
+        with patch.dict('sys.modules', {'typing': MagicMock()}):
+            if 'rxconf.types' in sys.modules:
+                del sys.modules['rxconf.types']
+            import rxconf.types
+            self.assertTrue(hasattr(rxconf.types, 'TypeAlias'))
+
+    @patch('sys.version_info', new=(3, 10, 1))
+    def test_import_typealias_from_typing_above_310(self):
+        with patch.dict('sys.modules', {'typing': MagicMock()}):
+            if 'rxconf.types' in sys.modules:
+                del sys.modules['rxconf.types']
+            import rxconf.types
+            self.assertTrue(hasattr(rxconf.types, 'TypeAlias'))
