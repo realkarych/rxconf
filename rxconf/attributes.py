4b7486df16eb738b3089e565f5a84a70e5056e3b
klukashka <klukin0202@yandex.ru>
2024-11-27 15:03:35 +0300

    __str__ method was edited to provide 'AttributeType(...)' for inner attributes

--- rxconf/attributes.py
+++ rxconf/attributes.py
@@ -32,7 +32,7 @@ class AttributeType(metaclass=ABCMeta):
 
     @exceptions.handle_unknown_exception
     def __str__(self) -> str:
-        return str(self._value)  # type: ignore
+        return f"AttributeType({self._value})"  # type: ignore
 
     @exceptions.handle_unknown_exception
     def __bool__(self) -> bool:
