9b7da838d260da391fc06b4dd869fd0f846e8440
klukashka <klukin0202@yandex.ru>
2024-12-01 18:43:57 +0300

    Codestyle changes

--- tests/unit/test_hashtools.py
+++ tests/unit/test_hashtools.py
@@ -20,11 +20,13 @@ _CONF_HASH_DIR = Path.cwd() / Path("tests/resources/conf_hashing")
 #     assert conf3 == conf4
 #     assert conf4 == conf5
 
+
 def test_same_attribute_structures():
     conf1 = RxConf.from_file(config_path=_CONF_HASH_DIR / "simple_structure.ini")
     conf2 = RxConf.from_file(config_path=_CONF_HASH_DIR / "simple_structure.yml")
 
     assert conf1 == conf2
+
 
 def test_list_and_set_to_differ():
     conf1 = RxConf.from_file(config_path=_CONF_HASH_DIR / "yaml_with_list.yaml")
@@ -32,11 +34,13 @@ def test_list_and_set_to_differ():
 
     assert conf1 != conf2
 
+
 def test_list_order_matters():
     conf1 = RxConf.from_file(config_path=_CONF_HASH_DIR / "list_123.toml")
     conf2 = RxConf.from_file(config_path=_CONF_HASH_DIR / "list_132.toml")
 
     assert conf1 != conf2
+
 
 def test_inner_structures():
     conf1 = RxConf.from_file(config_path=_CONF_HASH_DIR / "inner_structures.yml")
@@ -46,11 +50,13 @@ def test_inner_structures():
     assert conf1 == conf3
     assert conf1 == conf2
 
+
 def test_to_differ_str_and_int():
     conf1 = RxConf.from_file(config_path=_CONF_HASH_DIR / "simple_struct_with_str.yml")
     conf2 = RxConf.from_file(config_path=_CONF_HASH_DIR / "simple_structure.yml")
 
     assert conf1 != conf2
+
 
 def test_key_order():
     conf1 = RxConf.from_file(config_path=_CONF_HASH_DIR / "key_order_1_2.yml")
@@ -74,12 +80,14 @@ def test_key_order():
 #     assert conf3 == conf4
 #     assert conf4 == conf5
 
+
 @pytest.mark.asyncio
 async def test_async_same_attribute_structures():
     conf1 = await AsyncRxConf.from_file(config_path=_CONF_HASH_DIR / "simple_structure.ini")
     conf2 = await AsyncRxConf.from_file(config_path=_CONF_HASH_DIR / "simple_structure.yml")
 
     assert conf1 == conf2
+
 
 @pytest.mark.asyncio
 async def test_async_list_and_set_to_differ():
@@ -88,12 +96,14 @@ async def test_async_list_and_set_to_differ():
 
     assert conf1 != conf2
 
+
 @pytest.mark.asyncio
 async def test_async_list_order_matters():
     conf1 = await AsyncRxConf.from_file(config_path=_CONF_HASH_DIR / "list_123.toml")
     conf2 = await AsyncRxConf.from_file(config_path=_CONF_HASH_DIR / "list_132.toml")
 
     assert conf1 != conf2
+
 
 @pytest.mark.asyncio
 async def test_async_inner_structures():
@@ -104,12 +114,14 @@ async def test_async_inner_structures():
     assert conf1 == conf3
     assert conf1 == conf2
 
+
 @pytest.mark.asyncio
 async def test_async_to_differ_str_and_int():
     conf1 = await AsyncRxConf.from_file(config_path=_CONF_HASH_DIR / "simple_struct_with_str.yml")
     conf2 = await AsyncRxConf.from_file(config_path=_CONF_HASH_DIR / "simple_structure.yml")
 
     assert conf1 != conf2
+
 
 @pytest.mark.asyncio
 async def test_async_key_order():
