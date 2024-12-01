8394e33d3406881b8f46e48d1a25f006470d4b81
klukashka <klukin0202@yandex.ru>
2024-12-01 18:39:43 +0300

    Test architecture updates

--- tests/unit/test_hashtools.py
+++ tests/unit/test_hashtools.py
@@ -1,56 +1,119 @@
-import unittest
+import pytest
 from pathlib import Path
-from rxconf import RxConf
+from rxconf import RxConf, AsyncRxConf
 
 
 _RESOURCE_DIR = Path.cwd() / Path("tests/resources")
 _CONF_HASH_DIR = Path.cwd() / Path("tests/resources/conf_hashing")
 
 
-class TestComputeConfHash(unittest.TestCase):
-    # ---------- RxConf ------------
-    # def test_empty_to_equal(self):
-    #     conf1 = RxConf.from_file(config_path=_RESOURCE_DIR / "empty.ini")
-    #     conf2 = RxConf.from_file(config_path=_RESOURCE_DIR / "empty.yaml")
-    #     conf3 = RxConf.from_file(config_path=_RESOURCE_DIR / "empty.env")
-    #     conf4 = RxConf.from_file(config_path=_RESOURCE_DIR / "empty.toml")
-    #     conf5 = RxConf.from_file(config_path=_RESOURCE_DIR / "empty.json")
-    #
-    #     self.assertEqual(conf1 == conf2, True)
-    #     self.assertEqual(conf2 == conf3, True)
-    #     self.assertEqual(conf3 == conf4, True)
-    #     self.assertEqual(conf4 == conf5, True)
+# ---------- RxConf ------------
+# def test_empty_to_equal():
+#     conf1 = RxConf.from_file(config_path=_RESOURCE_DIR / "empty.ini")
+#     conf2 = RxConf.from_file(config_path=_RESOURCE_DIR / "empty.yaml")
+#     conf3 = RxConf.from_file(config_path=_RESOURCE_DIR / "empty.env")
+#     conf4 = RxConf.from_file(config_path=_RESOURCE_DIR / "empty.toml")
+#     conf5 = RxConf.from_file(config_path=_RESOURCE_DIR / "empty.json")
+#
+#     assert conf1 == conf2
+#     assert conf2 == conf3
+#     assert conf3 == conf4
+#     assert conf4 == conf5
 
-    def test_same_attribute_structures(self):
-        conf1 = RxConf.from_file(config_path=_CONF_HASH_DIR / "simple_structure.ini")
-        conf2 = RxConf.from_file(config_path=_CONF_HASH_DIR / "simple_structure.yml")
+def test_same_attribute_structures():
+    conf1 = RxConf.from_file(config_path=_CONF_HASH_DIR / "simple_structure.ini")
+    conf2 = RxConf.from_file(config_path=_CONF_HASH_DIR / "simple_structure.yml")
 
-        self.assertEqual(conf1 == conf2, True)
+    assert conf1 == conf2
 
-    def test_list_and_set_to_differ(self):
-        conf1 = RxConf.from_file(config_path=_CONF_HASH_DIR / "yaml_with_list.yaml")
-        conf2 = RxConf.from_file(config_path=_CONF_HASH_DIR / "yaml_with_set.yaml")
+def test_list_and_set_to_differ():
+    conf1 = RxConf.from_file(config_path=_CONF_HASH_DIR / "yaml_with_list.yaml")
+    conf2 = RxConf.from_file(config_path=_CONF_HASH_DIR / "yaml_with_set.yaml")
 
-        self.assertEqual(conf1 == conf2, False)
+    assert conf1 != conf2
 
-    def test_list_order_matters(self):
-        conf1 = RxConf.from_file(config_path=_CONF_HASH_DIR / "list_123.toml")
-        conf2 = RxConf.from_file(config_path=_CONF_HASH_DIR / "list_132.toml")
+def test_list_order_matters():
+    conf1 = RxConf.from_file(config_path=_CONF_HASH_DIR / "list_123.toml")
+    conf2 = RxConf.from_file(config_path=_CONF_HASH_DIR / "list_132.toml")
 
-        self.assertEqual(conf1 == conf2, False)
+    assert conf1 != conf2
 
-    def test_inner_structures(self):
-        conf1 = RxConf.from_file(config_path=_CONF_HASH_DIR / "inner_structures.yml")
-        conf2 = RxConf.from_file(config_path=_CONF_HASH_DIR / "inner_pseudo_changed.yml")
-        conf3 = RxConf.from_file(config_path=_CONF_HASH_DIR / "inner_structures.json")
+def test_inner_structures():
+    conf1 = RxConf.from_file(config_path=_CONF_HASH_DIR / "inner_structures.yml")
+    conf2 = RxConf.from_file(config_path=_CONF_HASH_DIR / "inner_pseudo_changed.yml")
+    conf3 = RxConf.from_file(config_path=_CONF_HASH_DIR / "inner_structures.json")
 
-        self.assertEqual(conf1 == conf3, True)
-        self.assertEqual(conf1 == conf2, False)
+    assert conf1 == conf3
+    assert conf1 == conf2
 
-    def test_to_differ_str_and_int(self):
-        conf1 = RxConf.from_file(config_path=_CONF_HASH_DIR / "simple_struct_with_str.yml")
-        conf2 = RxConf.from_file(config_path=_CONF_HASH_DIR / "simple_structure.yml")
+def test_to_differ_str_and_int():
+    conf1 = RxConf.from_file(config_path=_CONF_HASH_DIR / "simple_struct_with_str.yml")
+    conf2 = RxConf.from_file(config_path=_CONF_HASH_DIR / "simple_structure.yml")
 
-        self.assertEqual(conf1 == conf2, False)
-    # --------------------------------------
-    # ------------ AsyncRxConf -------------
+    assert conf1 != conf2
+
+def test_key_order():
+    conf1 = RxConf.from_file(config_path=_CONF_HASH_DIR / "key_order_1_2.yml")
+    conf2 = RxConf.from_file(config_path=_CONF_HASH_DIR / "key_order_2_1.yml")
+
+    assert conf1 != conf2
+
+# --------------------------------------
+# ------------ AsyncRxConf -------------
+
+# @pytest.mark.asyncio
+# async def test_async_empty_to_equal():
+#     conf1 = await AsyncRxConf.from_file(config_path=_RESOURCE_DIR / "empty.ini")
+#     conf2 = await AsyncRxConf.from_file(config_path=_RESOURCE_DIR / "empty.yaml")
+#     conf3 = await AsyncRxConf.from_file(config_path=_RESOURCE_DIR / "empty.env")
+#     conf4 = await AsyncRxConf.from_file(config_path=_RESOURCE_DIR / "empty.toml")
+#     conf5 = await AsyncRxConf.from_file(config_path=_RESOURCE_DIR / "empty.json")
+#
+#     assert conf1 == conf2
+#     assert conf2 == conf3
+#     assert conf3 == conf4
+#     assert conf4 == conf5
+
+@pytest.mark.asyncio
+async def test_async_same_attribute_structures():
+    conf1 = await AsyncRxConf.from_file(config_path=_CONF_HASH_DIR / "simple_structure.ini")
+    conf2 = await AsyncRxConf.from_file(config_path=_CONF_HASH_DIR / "simple_structure.yml")
+
+    assert conf1 == conf2
+
+@pytest.mark.asyncio
+async def test_async_list_and_set_to_differ():
+    conf1 = await AsyncRxConf.from_file(config_path=_CONF_HASH_DIR / "yaml_with_list.yaml")
+    conf2 = await AsyncRxConf.from_file(config_path=_CONF_HASH_DIR / "yaml_with_set.yaml")
+
+    assert conf1 != conf2
+
+@pytest.mark.asyncio
+async def test_async_list_order_matters():
+    conf1 = await AsyncRxConf.from_file(config_path=_CONF_HASH_DIR / "list_123.toml")
+    conf2 = await AsyncRxConf.from_file(config_path=_CONF_HASH_DIR / "list_132.toml")
+
+    assert conf1 != conf2
+
+@pytest.mark.asyncio
+async def test_async_inner_structures():
+    conf1 = await AsyncRxConf.from_file(config_path=_CONF_HASH_DIR / "inner_structures.yml")
+    conf2 = await AsyncRxConf.from_file(config_path=_CONF_HASH_DIR / "inner_pseudo_changed.yml")
+    conf3 = await AsyncRxConf.from_file(config_path=_CONF_HASH_DIR / "inner_structures.json")
+
+    assert conf1 == conf3
+    assert conf1 == conf2
+
+@pytest.mark.asyncio
+async def test_async_to_differ_str_and_int():
+    conf1 = await AsyncRxConf.from_file(config_path=_CONF_HASH_DIR / "simple_struct_with_str.yml")
+    conf2 = await AsyncRxConf.from_file(config_path=_CONF_HASH_DIR / "simple_structure.yml")
+
+    assert conf1 != conf2
+
+@pytest.mark.asyncio
+async def test_async_key_order():
+    conf1 = await AsyncRxConf.from_file(config_path=_CONF_HASH_DIR / "key_order_1_2.yml")
+    conf2 = await AsyncRxConf.from_file(config_path=_CONF_HASH_DIR / "key_order_2_1.yml")
+
+    assert conf1 != conf2
