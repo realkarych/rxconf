9b0208dc02b5f6efe66ff354fb29f5589383c324
klukashka <klukin0202@yandex.ru>
2024-12-01 22:28:34 +0300

    Changed function name

--- rxconf/hashtools.py
+++ rxconf/hashtools.py
@@ -3,7 +3,7 @@ from hashlib import sha256
 import typing as tp
 
 
-def _type_hashed(value: tp.Any) -> str:
+def _hash_with_type(value: tp.Any) -> str:
     return f"{type(value).__name__}{value}"
 
 
@@ -23,27 +23,27 @@ def compute_conf_hash(attribute: AttributeType, hash_sum: int = 0) -> int:
         for key in attribute._value:
             """ hash += hash(sum_of_the_hashes_inside_value) + hash(key) """
             val_sum = compute_conf_hash(attribute._value[key], 0)  # hash inside value
-            val_sum = _hash_to_int(_type_hashed(val_sum))  # hash(hash)
-            key_sum = _hash_to_int(_type_hashed(key))  # hash(key)
-            total_sum = _hash_to_int(_type_hashed(key_sum + val_sum))
+            val_sum = _hash_to_int(_hash_with_type(val_sum))  # hash(hash)
+            key_sum = _hash_to_int(_hash_with_type(key))  # hash(key)
+            total_sum = _hash_to_int(_hash_with_type(key_sum + val_sum))
             hash_sum += total_sum
 
     elif isinstance(attribute._value, set):
         set_sum = 0
         for elem in attribute._value:
-            set_sum += _hash_to_int(_type_hashed(elem._value))
+            set_sum += _hash_to_int(_hash_with_type(elem._value))
         set_sum += hashed_structures["set"]
-        hash_sum += _hash_to_int(_type_hashed(set_sum))
+        hash_sum += _hash_to_int(_hash_with_type(set_sum))
 
     elif isinstance(attribute._value, list):
         list_sum = 0
         for elem in attribute._value:
             list_sum += compute_conf_hash(elem, 0)
-            list_sum = _hash_to_int(_type_hashed(list_sum))  # to keep order
+            list_sum = _hash_to_int(_hash_with_type(list_sum))  # to keep order
         list_sum += hashed_structures["list"]
-        hash_sum += _hash_to_int(_type_hashed(list_sum))
+        hash_sum += _hash_to_int(_hash_with_type(list_sum))
 
     else:
-        hash_sum += _hash_to_int(_type_hashed(attribute._value))
+        hash_sum += _hash_to_int(_hash_with_type(attribute._value))
 
     return hash_sum
