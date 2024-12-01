165df87736030df595f2a3f070bd3fd0565a7961
klukashka <klukin0202@yandex.ru>
2024-12-01 18:38:00 +0300

    Modified hash function

--- rxconf/hashtools.py
+++ rxconf/hashtools.py
@@ -8,7 +8,7 @@ def _type_hashed(value: tp.Any) -> str:
 
 
 def _hash_to_int(value: str) -> int:
-    """Hash the value and return it as integer"""
+    """Hash the value and return it as an integer"""
     return int(sha256(value.encode('utf-8')).hexdigest(), 16)
 
 
@@ -19,30 +19,29 @@ hashed_structures: tp.Final[dict[str, int]] = {
 
 
 def compute_conf_hash(attribute: AttributeType, hash_sum: int = 0) -> int:
-
     if isinstance(attribute._value, dict):
         for key in attribute._value:
             """ hash += hash(sum_of_the_hashes_inside_value) + hash(key) """
-            hash_sum += compute_conf_hash(attribute._value[key], hash_sum)  # hash inside value
-            hash_sum += _hash_to_int(_type_hashed(hash_sum))  # hash(hash)
-            hash_sum += _hash_to_int(key)  # hash(key)
+            val_sum = compute_conf_hash(attribute._value[key], 0)  # hash inside value
+            val_sum = _hash_to_int(_type_hashed(val_sum))  # hash(hash)
+            key_sum = _hash_to_int(_type_hashed(key))  # hash(key)
+            total_sum = _hash_to_int(_type_hashed(key_sum + val_sum))
+            hash_sum += total_sum
 
     elif isinstance(attribute._value, set):
+        set_sum = 0
         for elem in attribute._value:
-            if isinstance(elem._value, (list, set, dict)):
-                hash_sum += compute_conf_hash(elem, hash_sum)
-            else:
-                hash_sum += _hash_to_int(_type_hashed(elem._value))
-        hash_sum += hashed_structures["set"]
+            set_sum += _hash_to_int(_type_hashed(elem._value))
+        set_sum += hashed_structures["set"]
+        hash_sum += _hash_to_int(_type_hashed(set_sum))
 
     elif isinstance(attribute._value, list):
+        list_sum = 0
         for elem in attribute._value:
-            if isinstance(elem._value, (list, set, dict)):
-                hash_sum += compute_conf_hash(elem, hash_sum)
-            else:
-                hash_sum = hash_sum + _hash_to_int(_type_hashed(elem._value))
-                hash_sum = _hash_to_int(_type_hashed(hash_sum))  # to ensure the order hasn't changed
-        hash_sum += hashed_structures["list"]
+            list_sum += compute_conf_hash(elem, 0)
+            list_sum = _hash_to_int(_type_hashed(list_sum))  # to keep order
+        list_sum += hashed_structures["list"]
+        hash_sum += _hash_to_int(_type_hashed(list_sum))
 
     else:
         hash_sum += _hash_to_int(_type_hashed(attribute._value))
