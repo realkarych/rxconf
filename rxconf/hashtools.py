86ba5e8f5e392eeb21be7752974e4f4beff30665
klukashka <klukin0202@yandex.ru>
2024-12-02 23:50:02 +0300

    Removed excess comments and renamed constant variable hash_structures

--- rxconf/hashtools.py
+++ rxconf/hashtools.py
@@ -12,7 +12,7 @@ def _hash_to_int(value: str) -> int:
     return int(sha256(value.encode('utf-8')).hexdigest(), 16)
 
 
-hashed_structures: tp.Final[dict[str, int]] = {
+HASHED_STRUCTURES: tp.Final[dict[str, int]] = {
     "list": _hash_to_int("[]"),
     "set": _hash_to_int("()"),
 }
@@ -21,10 +21,9 @@ hashed_structures: tp.Final[dict[str, int]] = {
 def compute_conf_hash(attribute: AttributeType, hash_sum: int = 0) -> int:
     if isinstance(attribute._value, dict):
         for key in attribute._value:
-            """ hash += hash(sum_of_the_hashes_inside_value) + hash(key) """
-            val_sum = compute_conf_hash(attribute._value[key], 0)  # hash inside value
-            val_sum = _hash_to_int(_hash_with_type(val_sum))  # hash(hash)
-            key_sum = _hash_to_int(_hash_with_type(key))  # hash(key)
+            val_sum = compute_conf_hash(attribute._value[key], 0)
+            val_sum = _hash_to_int(_hash_with_type(val_sum))
+            key_sum = _hash_to_int(_hash_with_type(key))
             total_sum = _hash_to_int(_hash_with_type(key_sum + val_sum))
             hash_sum += total_sum
 
@@ -32,15 +31,15 @@ def compute_conf_hash(attribute: AttributeType, hash_sum: int = 0) -> int:
         set_sum = 0
         for elem in attribute._value:
             set_sum += _hash_to_int(_hash_with_type(elem._value))
-        set_sum += hashed_structures["set"]
+        set_sum += HASHED_STRUCTURES["set"]
         hash_sum += _hash_to_int(_hash_with_type(set_sum))
 
     elif isinstance(attribute._value, list):
         list_sum = 0
         for elem in attribute._value:
             list_sum += compute_conf_hash(elem, 0)
-            list_sum = _hash_to_int(_hash_with_type(list_sum))  # to keep order
-        list_sum += hashed_structures["list"]
+            list_sum = _hash_to_int(_hash_with_type(list_sum))
+        list_sum += HASHED_STRUCTURES["list"]
         hash_sum += _hash_to_int(_hash_with_type(list_sum))
 
     else:
