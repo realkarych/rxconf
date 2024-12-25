26db63754b2d9181566a3252edbc709ab15fbc96
klukashka <klukin0202@yandex.ru>
2024-12-25 15:40:33 +0300

    VailtConfigBuilder was added

--- rxconf/config_builder.py
+++ rxconf/config_builder.py
@@ -1,7 +1,7 @@
 import abc
 import pathlib
 import typing as tp
-from rxconf.config_resolver import ConfigResolver, FileConfigResolver
+from rxconf.config_resolver import ConfigResolver, FileConfigResolver, VaultConfigResolver
 from rxconf.config_types import ConfigType, FileConfigType
 
 
@@ -39,3 +39,31 @@ class FileConfigBuilder(ConfigBuilder):
             path=path,
             encoding=encoding,
         )
+
+
+class VaultConfigBuilder(ConfigBuilder):
+
+    def __init__(self, config_resolver: VaultConfigResolver) -> None:
+        self._config_resolver: VaultConfigResolver = config_resolver
+
+    def build(
+        self,
+        token: str,
+        ip: str,
+        path: tp.Union[str, pathlib.PurePath],
+        ext: str
+    ) -> FileConfigType:
+        return self._config_resolver.resolve(ext=ext).load_from_vault(
+            token=token,
+            ip=ip,
+            path=path,
+        )
+
+    async def build_async(
+        self,
+        token: str,
+        ip: str,
+        path: tp.Union[str, pathlib.PurePath],
+        ext: str
+    ) -> FileConfigType:
+        pass
