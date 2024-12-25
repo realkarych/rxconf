a4c9a4f1cf5d4466752f3631332560b6d214f39e
klukashka <klukin0202@yandex.ru>
2024-12-25 15:41:25 +0300

    VaultConfigResolver was added with config resolving logic provided

--- rxconf/config_resolver.py
+++ rxconf/config_resolver.py
@@ -29,12 +29,41 @@ class FileConfigResolver(ConfigResolver):
 
         # TODO: add here link how to patch the extensions.
         raise exceptions.InvalidExtensionError(
-            f"Config file `{path}` has invalid extension: {extension}. "
+            f"Config file `{path}` has invalid extension: '{extension}'. "
+            f"If you want to support this extension, "
+            f"follow the tiny guideline: ..."
+        )
+
+
+class VaultConfigResolver(ConfigResolver):
+
+    def __init__(self, config_types: tp.Iterable[tp.Type[config_types.FileConfigType]]) -> None:
+        self._config_types = config_types
+
+    def resolve(
+        self,
+        ext: str,
+    ) -> tp.Type[config_types.FileConfigType]:
+        extension = "." + ext
+        for config_type in self._config_types:
+            if extension in config_type(
+                root_attribute=attributes.MockAttribute(),
+                path=pathlib.Path(),
+            ).allowed_extensions:
+                return config_type
+
+        # TODO: add here link how to patch the extensions.
+        raise exceptions.InvalidExtensionError(
+            f"Provided extension is invalid: '{ext}'. "
             f"If you want to support this extension, "
             f"follow the tiny guideline: ..."
         )
 
 
 DefaultFileConfigResolver: tp.Final[FileConfigResolver] = FileConfigResolver(
+    config_types=config_types.BASE_FILE_CONFIG_TYPES,
+)
+
+DefaultVaultConfigResolver: tp.Final[VaultConfigResolver] = VaultConfigResolver(
     config_types=config_types.BASE_FILE_CONFIG_TYPES,
 )
